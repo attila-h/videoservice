ec9d0785e473d3760c96dca4c6496ce96cf81805
attila-h <hauth.attila@gmail.com>
2020-11-26 18:55:41 +0100

    add video controller with route api/videos

---
+++ videoservice/src/main/java/com/codecool/videoservice/controller/VideoController.java
@@ -0,0 +1,26 @@
+package com.codecool.videoservice.controller;
+
+import com.codecool.videoservice.dao.interfaces.VideoDao;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@CrossOrigin
+@RestController
+public class VideoController {
+
+    private final VideoDao videoDao;
+
+    @Autowired
+    public VideoController(VideoDao videoDao) {
+        this.videoDao = videoDao;
+    }
+
+    @GetMapping("/api/videos")
+    private ResponseEntity getAllVideos() {
+        return ResponseEntity.ok(videoDao.getAll());
+    }
+
+}
