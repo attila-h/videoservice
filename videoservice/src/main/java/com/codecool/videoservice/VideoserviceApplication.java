10d23df5b4cbbb436f2b01d1f79c3e338476a878
attila-h <hauth.attila@gmail.com>
2020-11-27 16:30:37 +0100

    add configuration for VideoserviceApplication as Eureka client

--- videoservice/src/main/java/com/codecool/videoservice/VideoserviceApplication.java
+++ videoservice/src/main/java/com/codecool/videoservice/VideoserviceApplication.java
@@ -2,8 +2,10 @@ package com.codecool.videoservice;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 
 @SpringBootApplication
+@EnableEurekaClient
 public class VideoserviceApplication {
 
 	public static void main(String[] args) {
