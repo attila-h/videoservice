60ff0fc648fb603b92bd27ea9c83d75e49dd5c07
attila-h <hauth.attila@gmail.com>
2020-11-27 18:03:23 +0100

    add new module recommendation service

---
+++ recommendationservice/src/test/java/com/codecool/recommendationservice/RecommendationserviceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.codecool.recommendationservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RecommendationserviceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
